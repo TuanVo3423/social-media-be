3d3a62060007e73f39a9ef208edde083c29a0c3a
TuanVo3423 <tuanvanvo2003@gmail.com>
2024-02-02 20:34:20 +0700

    fix: validate error in upload single image

--- src/controllers/medias.controller.ts
+++ src/controllers/medias.controller.ts
@@ -1,19 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
-import formidable from 'formidable'
-import path from 'path'
+import { handleUploadSingleImage } from '~/utils/file'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
   // upload into folder upload
-  const form = formidable({
-    uploadDir: path.resolve('uploads'),
-    maxFiles: 1,
-    maxFieldsSize: 300 * 1024,
-    keepExtensions: true
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    return res.json({ message: 'upload successfully!' })
+  const data = await handleUploadSingleImage(req)
+  return res.json({
+    message: 'Upload success'
   })
 }
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,3 +1,5 @@
+import { Request } from 'express'
+import formidable from 'formidable'
 import fs from 'fs'
 import path from 'path'
 export const initFolder = () => {
@@ -6,4 +8,32 @@ export const initFolder = () => {
       recursive: true
     })
   }
+}
+
+export const handleUploadSingleImage = async (req: Request) => {
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    maxFileSize: 3000 * 1024, //3000KB
+    keepExtensions: true,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image'))
+      if (!valid) {
+        form.emit('error' as any, new Error('Invalid file type') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('Invalid file'))
+      }
+      return resolve(files)
+    })
+  })
 }
