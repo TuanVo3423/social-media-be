c9014dea986b2b052f69e57067d6ae3d980b521e
TuanVo3423 <tuanvanvo2003@gmail.com>
2024-02-23 20:39:52 +0700

    refactor: tweet schema for create tweet services

--- src/models/schemas/Tweet.schema.ts
+++ src/models/schemas/Tweet.schema.ts
@@ -4,16 +4,16 @@ import { Media } from '../Others'
 
 interface ITweet {
   _id?: ObjectId
-  user_id: ObjectId
+  user_id: string
   type: TweetType
   audience: TweetAudience
   content: string
-  parent_id: null | ObjectId //  chỉ null khi tweet gốc
+  parent_id: null | string //  chỉ null khi tweet gốc
   hashtags: ObjectId[]
-  mentions: ObjectId[]
+  mentions: string[]
   medias: Media[]
-  guest_views: number
-  user_views: number
+  guest_views?: number
+  user_views?: number
   created_at?: Date
   updated_at?: Date
 }
@@ -35,16 +35,16 @@ export default class Tweet {
   constructor(tweet: ITweet) {
     const date = new Date()
     this._id = tweet._id
-    this.user_id = tweet.user_id
+    this.user_id = new ObjectId(tweet.user_id)
     this.type = tweet.type
     this.audience = tweet.audience
     this.content = tweet.content
-    this.parent_id = tweet.parent_id
+    this.parent_id = tweet.parent_id ? new ObjectId(tweet.parent_id) : null
     this.hashtags = tweet.hashtags
-    this.mentions = tweet.mentions
+    this.mentions = tweet.mentions.map((mention) => new ObjectId(mention))
     this.medias = tweet.medias
-    this.guest_views = tweet.guest_views
-    this.user_views = tweet.user_views
+    this.guest_views = tweet.guest_views || 0
+    this.user_views = tweet.user_views || 0
     this.created_at = tweet.created_at || date
     this.updated_at = tweet.updated_at || date
   }
