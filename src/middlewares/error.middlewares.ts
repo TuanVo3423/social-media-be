339734a6fdb96f294d5ded2df91d9b4a300306a5
TuanVo3423 <tuanvanvo2003@gmail.com>
2024-05-03 09:08:30 +0700

    refactor: detect case error of aws

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -4,16 +4,32 @@ import { HTTP_STATUS } from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/Errors'
 
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, 'status'))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, 'status'))
+    }
+    const finalError: any = {}
+
+    // lấy các key của err, nếu key đó không phải là enumerable hoặc configurable thì bỏ qua (loi do aws khong cho overide error)
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.enumerable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable
+      ) {
+        return
+      }
+      finalError[key] = err[key]
+    })
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errorInfo: omit(finalError, 'stack')
+    })
+  } catch (error) {
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errorInfo: omit(error as any, 'stack')
+    })
   }
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, 'stack')
-  })
 }
 // lỗi có status đi kèm -> lỗi của mình custom
 // lỗi không có status đi kèm => mặc định sẽ là 500, lỗi server -> lỗi của trình duyệt trả về, không custom
